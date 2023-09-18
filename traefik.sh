c31add8b892dfa310a38b278a711745be7f97648
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-09-19 10:50:40 +1200

    Create traefik.sh

---
+++ traefik.sh
@@ -0,0 +1,2 @@
+echo "This file serves no purpose other than to generate the language summary in the gihub repo."
+echo "If you found this, here's your treat: https://youtube.com/watch?v=EE-xtCF3T94"
